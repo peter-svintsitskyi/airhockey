e3da2ca6e2f8ebe5fd8e7d59b1dc23fba6a2493c
Petro Svintsitskyi <psvintsitskyi@rebbix.com>
2018-11-22 16:49:05 +0200

    trajectory calculation

--- hsv.py
+++ hsv.py
@@ -4,6 +4,26 @@ from sklearn import preprocessing
 import math
 
 
+def get_intersect(a1, a2, b1, b2):
+    """ 
+    Returns the point of intersection of the lines passing through a2,a1 and b2,b1.
+    a1: [x, y] a point on the first line
+    a2: [x, y] another point on the first line
+    b1: [x, y] a point on the second line
+    b2: [x, y] another point on the second line
+    """
+
+    s = np.vstack([a1,a2,b1,b2])        # s for stacked
+    h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
+    l1 = np.cross(h[0], h[1])           # get first line
+    l2 = np.cross(h[2], h[3])           # get second line
+    x, y, z = np.cross(l1, l2)          # point of intersection
+    if z == 0:                          # lines are parallel
+        return (float('inf'), float('inf'))
+
+    return (x/z, y/z)
+
+
 class Tracker(object):
     x = None
     y = None
@@ -39,12 +59,6 @@ class WorldToFrameTranslator(object):
     def __init__(self, frame_size, table_size):
         self.frame_width, self.frame_height = frame_size
         self.table_width, self.table_height = table_size
-
-
-        print (frame_size)
-
-
-
         self.horizontal_margin = 10
         frame_field_width = self.frame_width - self.horizontal_margin * 2
         frame_field_height = frame_field_width / 2
@@ -108,17 +122,64 @@ class PuckInfo(object):
         self.position = position
 
 
-class Game(object):
+class Robot(object):
+    pass
+
+
+class GameStrategy(object):
+    pass
+
+
+class World(object):
     def __init__(self, table_size):
+        self.table_width, self.table_height = table_size
         self.table_size = table_size
+        self.trajectory = [
+            (100, 100)
+        ]
 
     def tick(self, puck_info):
         self.puck_info = puck_info
 
+        p_x, p_y = puck_info.position
+        v_x, v_y = puck_info.vector
+
+        self.trajectory = []
+        for x in range(0, int(self.table_width / 2), 100):
+            intersection = get_intersect(
+                (x, 0), (x, 1),
+                puck_info.position, (p_x + v_x, p_y + v_y)
+            )
+
+            i_x, i_y = intersection
+
+            if i_x == float('inf'):
+                continue
+
+            d_x = i_x - p_x
+            d_y = i_y - p_y
+
+            distance = math.sqrt(d_x*d_x + d_y*d_y)
+
+            if puck_info.velocity != 0:
+                time = distance / puck_info.velocity
+            else:
+                time = 'Infinity'
+
+            n = int(i_y / self.table_height)
+            if i_y < 0 or i_y > self.table_height:
+                i_y = abs(i_y) - abs(n * self.table_height)
+
+                if n % 2 != 0:
+                    i_y = self.table_height - i_y
+
+            self.trajectory.append(((i_x, i_y), time))
+
     def get_debug(self):
         return dict(
             puck_info=self.puck_info,
-            table_size=self.table_size
+            table_size=self.table_size,
+            trajectory=self.trajectory
         )
 
 
@@ -145,6 +206,12 @@ class Debug(object):
         # table
         cv2.rectangle(frame, translator.w2f((0, 0)), translator.w2f(debug_info['table_size']), (0, 255, 255), 2)
 
+        # trajectory
+        for intersection in debug_info['trajectory']:
+            point, velocity = intersection
+            cv2.circle(frame, translator.w2f(point), 10, (0, 0, 255), -1)
+            cv2.putText(frame, str(velocity), translator.w2f(point), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 127, 255), thickness=3)
+
         # debug
         cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('frame', 600, 600)
@@ -156,7 +223,7 @@ table_size = (1200, 600)
 cap = cv2.VideoCapture(0)
 translator = WorldToFrameTranslator((cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), table_size)
 tracker = Tracker()
-game = Game(table_size)
+world = World(table_size)
 debug = Debug(translator)
 
 while(1):
@@ -174,9 +241,16 @@ while(1):
         continue
 
     vector, velocity = track
-    game.tick(PuckInfo(puck_position, vector, velocity))
 
-    debug.draw(frame, game.get_debug())
+    # puck_position = (900, 300)
+    # vector = preprocessing.normalize(
+    #     np.asarray([-1, -2.2]).reshape(1, -1)
+    # )[0]
+    # velocity = 100
+
+    world.tick(PuckInfo(puck_position, vector, velocity))
+
+    debug.draw(frame, world.get_debug())
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
